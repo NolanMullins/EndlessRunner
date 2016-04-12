d88593c0171402337a979e732359a7a297e42e00
NolanMullins <nolan.mullins@me.com>
2016-04-12 12:24:03 -0400

    jumping

--- EndlessRunner/Assets/Scripts/PlayerController.cs
+++ EndlessRunner/Assets/Scripts/PlayerController.cs
@@ -3,13 +3,45 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D myBody;
+
+    public bool grounded;
+    public bool doubleJump;
+    public LayerMask whatIsGround;
+
+    private Collider2D myCollider;
+
 	// Use this for initialization
 	void Start () {
-	
+        myBody = GetComponent<Rigidbody2D>();
+        doubleJump = false;
+        myCollider = GetComponent<Collider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        //check if the players hitting ground
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+        myBody.velocity = new Vector2(moveSpeed, myBody.velocity.y);
+
+        //jumping
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            if (grounded)
+            {
+                doubleJump = false;
+                myBody.velocity = new Vector2(myBody.velocity.x, jumpForce);
+            }
+            else if (!doubleJump)
+            {
+                doubleJump = true;
+                myBody.velocity = new Vector2(myBody.velocity.x, jumpForce);
+            }
+        }
 	}
 }
